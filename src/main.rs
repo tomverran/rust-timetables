b93e2d68fa39dc07ef3c0679f0e6f93316d091cd
tomverran <tom@tomverran.co.uk>
2017-02-20 21:00:47 +0000

    Initial basic parsing

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
 extern crate ftp;
-extern crate tempfile;
 extern crate flate2;
+extern crate xml;
 
 use ftp::FtpStream;
 use ftp::types::FtpError;
 use ftp::types::FileType;
-use std::io;
 use std::str;
 use std::env;
-use std::fs::File;
-use std::io::Write;
 use std::io::Read;
-use std::io::Seek;
-use std::io::SeekFrom;
+use std::io::BufReader;
 use std::io::Error;
 use flate2::read::GzDecoder;
 use std::convert::From;
+use xml::reader::{EventReader, XmlEvent};
+use xml::attribute::OwnedAttribute;
 
 #[derive(Debug)]
 enum TimetableError {
     Ftp(FtpError),
     IO(Error),
     NoTimetable
+}
+
+#[derive(Debug)]
+struct Train {
+    id: String,
+    shortlands_time: String,
+    victoria_time: String
 }
 
 impl From<FtpError> for TimetableError {
@@ -38,7 +43,6 @@ impl From<Error> for TimetableError {
 // the main either type of this entire program
 type TimetableResult<A> = Result<A, TimetableError>;
 
-
 // given a list of files from FTP, try to find the timetable
 fn find_v8_timetable(v: Vec<String>) -> TimetableResult<String> {
     v.into_iter()
@@ -46,21 +50,73 @@ fn find_v8_timetable(v: Vec<String>) -> TimetableResult<String> {
         .ok_or(TimetableError::NoTimetable)
 }
 
-fn download_file(mut ftp: FtpStream, file: String) -> TimetableResult<File> {
-    ftp.transfer_type(FileType::Binary)?;
-    return ftp.retr(&file, |r| {
-        let mut temp = tempfile::tempfile().map_err(FtpError::ConnectionError)?;
-        io::copy(r, Write::by_ref(&mut temp)).map_err(FtpError::ConnectionError)?;
-        temp.seek(SeekFrom::Start(0)).map_err(FtpError::ConnectionError)?;
-        Result::Ok(temp)
-    }).map_err(TimetableError::Ftp);
+fn download_and_parse_file(mut ftp: FtpStream, file: String) -> TimetableResult<Vec<Train>> {
+    ftp.transfer_type(FileType::Binary).map_err(TimetableError::Ftp)?;
+    ftp.retr(&file, |r| GzDecoder::new(r).map(parse_file).map_err(FtpError::ConnectionError)) 
+    .map_err(TimetableError::Ftp)
+}
+
+fn is_shortlands(e: &Vec<OwnedAttribute>) -> bool {
+    e.iter()
+    .filter(|a| a.name.local_name == "tpl")
+    .filter(|a| a.value.starts_with("SHRTLND"))
+    .count() != 0
+}
+
+fn is_victoria(e: &Vec<OwnedAttribute>) -> bool {
+    e.iter()
+    .filter(|a| a.name.local_name == "tpl")
+    .filter(|a| a.value.starts_with("VIC"))
+    .count() != 0
+}
+
+fn parse_file<R: Read>(f: R) -> Vec<Train> {
+    
+    let buf_reader = BufReader::new(f);
+    let parser = EventReader::new(buf_reader);
+    
+    // trains we've found so far
+    let mut trains: Vec<Train> = Vec::new();
+    
+    // the current train we're looking at
+    let mut train_id: Option<String> = None;
+    let mut shortlands_time: Option<String> = None; 
+    let mut terminus_time: Option<String> = None;
+    
+    for e in parser {
+        match e {
+            
+            Ok(XmlEvent::StartElement { ref name, ref attributes, .. }) if name.local_name == "Journey" => {
+                train_id = attributes.into_iter()
+                    .find(|a| a.name.local_name == "trainId")
+                    .map(|v| v.to_owned().value);
+                shortlands_time = None;
+                terminus_time = None;
+            }
+            
+            Ok(XmlEvent::StartElement { ref attributes, .. }) if is_victoria(attributes) => {
+                terminus_time = attributes.iter().find(|a| a.name.local_name == "pta").map(|v| v.to_owned().value);
+            }
+            
+            Ok(XmlEvent::StartElement { ref attributes, .. }) if is_shortlands(attributes) => {
+                shortlands_time = attributes.iter().find(|a| a.name.local_name == "pta").map(|v| v.to_owned().value);
+            }
+            
+            Ok(XmlEvent::EndElement { ref name }) if name.local_name == "Journey" => {
+                if shortlands_time.is_some() && terminus_time.is_some() {
+                    println!("/{:?} srt: {:?} vic: {:?}", train_id, shortlands_time, terminus_time);
+                }
+            }
+            
+            _ => {}
+        }
+    }
+    trains
 }
 
 fn main() {
-
     let password = env::var("FTP_PASSWORD").unwrap();
     let ftp_result = FtpStream::connect("datafeeds.nationalrail.co.uk:21");
-    let mut output = File::create("output.xml").unwrap();
 
     let r = ftp_result
     .map_err(TimetableError::Ftp)
@@ -69,15 +125,7 @@ fn main() {
         .and_then(|_| ftp.nlst(None))
         .map_err(TimetableError::Ftp)
         .and_then(find_v8_timetable)
-        .and_then(|f| download_file(ftp, f))
-        .and_then(|f|
-            GzDecoder::new(f)
-            .map_err(TimetableError::IO)
-        )
-        .and_then(|mut f|
-            io::copy(Read::by_ref(&mut f), Write::by_ref(&mut output))
-            .map_err(TimetableError::IO)
-        )
+        .and_then(|f| download_and_parse_file(ftp, f))
     });
     println!("{:?}", r);
 }
